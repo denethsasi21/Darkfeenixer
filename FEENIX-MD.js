284364b9e521d5eed749d17ebb036e7028f95cbe
Darkfeenixer <162316769+Darkfeenixer@users.noreply.github.com>
2024-03-05 21:13:05 +0530

    Create FEENIX-MD.js

---
+++ FEENIX-MD.js
@@ -0,0 +1,8 @@
+Project Name : FEENIX - MD
+Creator : @darkfeenixer , @mr-kasun-x 
+
+This is Whatsapp Bot Project Created By Using Baileys web Api.
+❌ You Are not Allowed To Edit This File.
+❌ You Are Not Allowed To Get This File.
+❌ You Are Not Allowed To Create Clone Version Using This Bot.
+
